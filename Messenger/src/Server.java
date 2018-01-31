3547a37f50e959f7b14e5200a399221e3e74bd28
Tmax <Tmax@DESKTOP-9FN01BS>
2018-01-31 17:50:51 +0900

    20180131 수정

--- Messenger/src/Server.java
+++ Messenger/src/Server.java
@@ -1,4 +1,6 @@
+
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Collections;
@@ -11,11 +13,39 @@ public class Server {
 	Server() {
 		client = Collections.synchronizedMap(new HashMap<String, DataOutputStream>());
 	}
-	
-	
+
+	public void start() {
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+
+		try {
+			serverSocket = new ServerSocket(7777);
+			System.out.println("Server Start");
+
+			while (true) {
+				socket = serverSocket.accept();
+				System.out.println("[" + socket.getInetAddress() //
+						+ ":" + socket.getPort() + "]" + "에서 접속하였습니다.");
+
+				// 서버에서 클라이언트로 메시지를 전송할 Thread 생성
+				ServerReceiver thread = new ServerReceiver(socket,client);
+				thread.start();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	} // start()
 
 	public static void main(String[] args) {
-
+		new Server().start();
 	}
-
 }
+
+
---
+++ Messenger/src/ServerReceiver.java
@@ -0,0 +1,50 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.Map;
+
+class ServerReceiver extends Thread {
+	Socket socket;
+	DataInputStream in;
+	DataOutputStream out;
+	Map<String, DataOutputStream> client;
+
+	ServerReceiver(Socket socket, Map<String, DataOutputStream> client) {
+		this.socket = socket;
+		this.client = client;
+		try {
+			in = new DataInputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+		} catch (IOException e) {
+		}
+	}
+
+	void BroadCast(String msg) {
+		Iterator<String> it = client.keySet().iterator();
+
+		while (!it.hasNext()) {
+			String name = it.next();
+			DataOutputStream out = client.get(name);
+			try {
+				out.writeUTF(msg);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void run() {
+		String Client_name = null;
+		try {
+			Client_name = in.readUTF();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		BroadCast("Enter Client : " + Client_name);
+	}
+
+}
